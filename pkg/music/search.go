f42c8e2737798c7777cd051c6a078588e8b4c67d
Turgho <victor.hugo3692111@gmail.com>
2026-05-09 01:53:47 -0300

    Improves audio download reliability
    Enables the use of cookies during audio downloads, which can help in bypassing restrictions or maintaining session state for higher success rates.
    
    Updates the YouTube player client from `tv_simply` to `tv` for enhanced stability and compatibility, particularly in cloud environments.

--- pkg/music/search.go
+++ pkg/music/search.go
@@ -41,7 +41,7 @@ func DownloadAudio(ctx context.Context, query string) ([]byte, string, error) {
 
 	args := []string{
 		input,
-		// "--cookies", "cookies.txt",
+		"--cookies", "cookies.txt",
 
 		"--verbose",
 		"--list-formats",
@@ -52,7 +52,7 @@ func DownloadAudio(ctx context.Context, query string) ([]byte, string, error) {
 		"--no-playlist",   // Evita baixar playlists inteiras sem querer
 
 		// Usa client web (mais estável que android no Square Cloud)
-		"--extractor-args", "youtube:player_client=tv_simply",
+		"--extractor-args", "youtube:player_client=tv",
 
 		"--user-agent", "Mozilla/5.0", // Evita bloqueios simples de bot detection
 		"--socket-timeout", "15", // Timeout para evitar travamentos em rede lenta
